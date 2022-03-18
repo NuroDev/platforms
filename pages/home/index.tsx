8d95dab1e149f9f84ce5877d6098a5efa0cfc6fe
nurodev <me@nuro.dev>
2022-03-18 17:19:59 +0000

    Added homepage app redirect

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,14 +10,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="m-auto w-48">
-        <Image
-          width={512}
-          height={512}
-          src="/logo.png"
-          alt="Platforms on Vercel"
-        />
-      </div>
+      <Link href="/app" passHref>
+        <a className="m-auto w-48" rel="noreferrer noopener">
+          <Image
+            width={512}
+            height={512}
+            src="/logo.png"
+            alt="Platforms on Vercel"
+          />
+        </a>
+      </Link>
     </div>
   );
 }
